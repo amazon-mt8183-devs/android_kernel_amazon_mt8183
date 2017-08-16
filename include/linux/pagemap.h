b3022d1cfa2fa5be21cf56228da7b59a3fef8278
Sami Tolvanen <samitolvanen@google.com>
2017-08-16 14:38:13 -0700

    BACKPORT: mm: fix filler function type mismatch
    Bug: 67506682
    Change-Id: I6f615164ccd86b407540ada9bbcb39d910395db9
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    (cherry picked from commit 4fd840d1743308b2ef470534523009dd99b3ce2b)
    Signed-off-by: Dan Aloni <daloni@magicleap.com>
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- include/linux/pagemap.h
+++ include/linux/pagemap.h
@@ -248,7 +248,7 @@ static inline struct page *page_cache_alloc_readahead(struct address_space *x)
 #endif
 }
 
-typedef int filler_t(void *, struct page *);
+typedef int filler_t(struct file *, struct page *);
 
 pgoff_t page_cache_next_hole(struct address_space *mapping,
 			     pgoff_t index, unsigned long max_scan);
@@ -389,7 +389,7 @@ extern int read_cache_pages(struct address_space *mapping,
 static inline struct page *read_mapping_page(struct address_space *mapping,
 				pgoff_t index, void *data)
 {
-	filler_t *filler = (filler_t *)mapping->a_ops->readpage;
+	filler_t *filler = mapping->a_ops->readpage;
 	return read_cache_page(mapping, index, filler, data);
 }
 
